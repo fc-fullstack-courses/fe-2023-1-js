429a57a9455bf1202f53bae2fccccbd5ca56200d
Kyrylo <fresh.js.ftd.@gmail.com>
2023-04-05 18:37:27 +0300

    Add basic data types

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+console.log('String 1');
+console.log("false");
+console.log(`String 3`);
+console.log(42.35);
+console.log(5053n);
+console.log(true);
+console.log(false);
+console.log(null);
+console.log(undefined);
