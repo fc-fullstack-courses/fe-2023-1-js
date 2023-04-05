6d9b8cdbc82d4066cd922a3affc81b8cfc59eabc
Kyrylo <fresh.js.ftd.@gmail.com>
2023-04-05 19:48:17 +0300

    Add variables example

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,6 +4,7 @@
 comment
     comment
 */
+/*
 console.log('String 1'); // String
 console.log("false");
 console.log(`String 3`);
@@ -13,3 +14,44 @@ console.log(true); // Boolean
 console.log(false);
 console.log(null); // null
 console.log(undefined); // undefined
+*/
+
+// оголошення змінної
+let username;
+
+// // зміна значення змінної / присвоєння
+username = 'Kyrylo';
+
+// console.log(username); // вивід значень
+
+// ініціалізація змінної
+let username2 = 'Anton';
+// console.log(username2);
+
+/*
+  за допомогою let , const
+  1. ініціалізуємо змінні
+  2. дивимось їх значення
+  3. перевизначте значення
+  4. подивіться знову
+*/
+
+let value1 = 10;
+console.log(value1);
+value1 = 'test';
+console.log(value1);
+
+const value2 = 100;
+console.log(value2);
+// value2 = 500;
+console.log(value2);
+
+let _; // lodash
+let $; // JQuery
+console.log(5000000)
+// error
+// let 1321313asfdsfd;
+// let test-name;
+
+let longVariableName;
+
