2b7ff9bffff2f0def377ff0949c859248f5ec450
Kyrylo <fresh.js.ftd.@gmail.com>
2023-04-05 20:57:40 +0300

    Add some type conversion examples

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,57 +1,19 @@
-// comment
-/*
-  comment
-comment
-    comment
-*/
-/*
-console.log('String 1'); // String
-console.log("false");
-console.log(`String 3`);
-console.log(42.35); // Number
-console.log(5053n); // BigInt
-console.log(true); // Boolean
-console.log(false);
-console.log(null); // null
-console.log(undefined); // undefined
-*/
+// console.log((0.1 / 100 + 0.2 / 100) * 100);
+// console.log(100 / 0);
+// console.log(100 / -0);
 
-// оголошення змінної
-let username;
+// console.log(NaN); // Not a Number
+// console.log(NaN / NaN);
 
-// // зміна значення змінної / присвоєння
-username = 'Kyrylo';
+let number1 = 100;
+number1 = '0';
 
-// console.log(username); // вивід значень
+let number2 = 20;
+number2 = '50';
 
-// ініціалізація змінної
-let username2 = 'Anton';
-// console.log(username2);
 
-/*
-  за допомогою let , const
-  1. ініціалізуємо змінні
-  2. дивимось їх значення
-  3. перевизначте значення
-  4. подивіться знову
-*/
+console.log(0 / 0);
 
-let value1 = 10;
-console.log(value1);
-value1 = 'test';
-console.log(value1);
-
-const value2 = 100;
-console.log(value2);
-// value2 = 500;
-console.log(value2);
-
-let _; // lodash
-let $; // JQuery
-console.log(5000000)
-// error
-// let 1321313asfdsfd;
-// let test-name;
-
-let longVariableName;
-
+let string1 = '';
+let string2 = true;
+console.log(string1 + string2);
