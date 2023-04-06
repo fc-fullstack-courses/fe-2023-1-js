d1de50d27add28cb1deeece428260fa498613309
Kyrylo <fresh.js.ftd.@gmail.com>
2023-04-06 21:25:50 +0300

    Add if example

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,56 +1,108 @@
-// console.log(typeof null);
-/* Math */
-// console.log(270 / 4);
-// console.log(270 % 4);
-// console.log(270 * 4);
-// console.log((-2) ** 2);
+// // console.log(typeof null);
+// /* Math */
+// // console.log(270 / 4);
+// // console.log(270 % 4);
+// // console.log(270 * 4);
+// // console.log((-2) ** 2);
+// /*
+// let counter = 5;
+// console.log(counter);
+// console.log(++counter);
+// console.log(counter);
+// console.log(counter++);
+// console.log(counter);
 
-let counter = 5;
-console.log(counter);
-console.log(++counter);
-console.log(counter);
-console.log(counter++);
-console.log(counter);
+// console.log(+10);
+// console.log(-100);
+// let value = '0xFF';
+// console.log(+value); // перевести до числа
+// */
+// /* Логічні оператори */
+// console.log(!false);
+// console.log(!!true);
+// let notBoolean = '';
+// console.log(!!notBoolean); // перевести до булевеого
 
-console.log(+10);
-console.log(-100);
-let value = '0xFF';
-console.log(+value); // перевести до числа
+// let maxValue = 100;
+// let data = 50000000;
+// console.log(10 < 5);
+// console.log(data <= maxValue);
+// console.log('test' > 'text');
+// console.log('============================')
+// let number = -305;
+// console.log(number >= 0);
+// console.log(number <= 100);
+// console.log(number >= 0 && number <= 100); // лог І
+// console.log(number >= 0 || number <= 100); // лог АБО
+// // console.log(number >= 0 || number <= -500000);
+// console.log( NaN && undefined && false);
+// console.log( NaN || 5 || 10);
+// // bad
+// // console.log(500 != 5000); // true
+// // console.log(100 == 100); // true
+// // console.log(50 == '50'); // true
 
-/* Логічні оператори */
-console.log(!false);
-console.log(!!true);
-let notBoolean = '';
-console.log(!!notBoolean); // перевести до булевеого
+// console.log( 50 === '50'); // false
+// console.log( 50 !== 100); // true
+// console.log('======= Присвоєння ==================');
+// let test1;
+// test1 = 'asddsfgfdd';
+// let number1 = 30;
 
-let maxValue = 100;
-let data = 50000000;
-console.log(10 < 5);
-console.log(data <= maxValue);
-console.log('test' > 'text');
-console.log('============================')
-let number = -305;
-console.log(number >= 0);
-console.log(number <= 100);
-console.log(number >= 0 && number <= 100); // лог І
-console.log(number >= 0 || number <= 100); // лог АБО
-// console.log(number >= 0 || number <= -500000);
-console.log( NaN && undefined && false);
-console.log( NaN || 5 || 10);
-// bad
-// console.log(500 != 5000); // true
-// console.log(100 == 100); // true
-// console.log(50 == '50'); // true
+// // number1 = number1 + 10;
+// // number1 += 10;
+// number1 /= 3;
 
-console.log( 50 === '50'); // false
-console.log( 50 !== 100); // true
-console.log('======= Присвоєння ==================');
-let test1;
-test1 = 'asddsfgfdd';
-let number1 = 30;
+// console.log(number1);
 
-// number1 = number1 + 10;
-// number1 += 10;
-number1 /= 3;
+console.log('first');
 
-console.log(number1);
+let condition = false;
+
+let a;
+let b;
+
+// alert(condition);
+let userAnswer;
+userAnswer = prompt('Це ми дали промпту');
+
+console.log(userAnswer);
+
+if (a > b) {
+  // особлива зона
+  // виконується тількі якщо вираз в () істиний
+  console.log('second');
+} else if (b > a) {
+  // виконується якщо блоки перед ним не виконалися
+  // і якщо його вираз істинний
+  console.log('else if');
+} else {
+  // виконується коли інші умови не виколалися
+  console.log('condition was false');
+}
+
+/*
+  в файлі записати в змінну якийсь пароль
+  при запуску сторінки спитати у користувача пароль
+  якщо пароль збігається з паролем який збережено у файлі
+  то сказати користувачу що він увійшов в систему
+  якщо не збігається- то сказати що він не війшов
+*/
+
+// if () {
+
+// } else {}
+
+// if () {
+
+// } else if {}
+
+// if () {
+
+// } else if {
+
+// } else {
+
+// }
+
+console.log('third');
