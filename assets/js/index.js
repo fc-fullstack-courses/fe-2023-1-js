3cea0e476a1e476d339e2b53b0b21082fb230e08
Kyrylo <fresh.js.ftd.@gmail.com>
2023-04-06 20:04:20 +0300

    Add js operators examples

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,8 +12,45 @@ console.log(counter);
 console.log(counter++);
 console.log(counter);
 
-
 console.log(+10);
 console.log(-100);
 let value = '0xFF';
-console.log(+value);
+console.log(+value); // перевести до числа
+
+/* Логічні оператори */
+console.log(!false);
+console.log(!!true);
+let notBoolean = '';
+console.log(!!notBoolean); // перевести до булевеого
+
+let maxValue = 100;
+let data = 50000000;
+console.log(10 < 5);
+console.log(data <= maxValue);
+console.log('test' > 'text');
+console.log('============================')
+let number = -305;
+console.log(number >= 0);
+console.log(number <= 100);
+console.log(number >= 0 && number <= 100); // лог І
+console.log(number >= 0 || number <= 100); // лог АБО
+// console.log(number >= 0 || number <= -500000);
+console.log( NaN && undefined && false);
+console.log( NaN || 5 || 10);
+// bad
+// console.log(500 != 5000); // true
+// console.log(100 == 100); // true
+// console.log(50 == '50'); // true
+
+console.log( 50 === '50'); // false
+console.log( 50 !== 100); // true
+console.log('======= Присвоєння ==================');
+let test1;
+test1 = 'asddsfgfdd';
+let number1 = 30;
+
+// number1 = number1 + 10;
+// number1 += 10;
+number1 /= 3;
+
+console.log(number1);
