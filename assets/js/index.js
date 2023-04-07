79b8bc8ab54ce1df89543e21d91d91508b507189
Kyrylo <fresh.js.ftd.@gmail.com>
2023-04-07 20:49:23 +0300

    Add basic function

---
+++ assets/js/index.js
@@ -0,0 +1,22 @@
+// alert
+// prompt
+// console.log
+
+// alert('parameter'); // функція з параметром
+
+// functional expression - функціональний вираз
+const myFunction = function () {
+  // тіло функціі - виконуюєтьс коли функцію запускають
+
+  const userInput = prompt('Введіть ваше ім`я');
+
+  const greetingText = 'Hello ' + userInput;
+
+  console.log(greetingText);
+};
+
+myFunction();
+// myFunction();
+// myFunction();
+
+const userInput = 12345;
