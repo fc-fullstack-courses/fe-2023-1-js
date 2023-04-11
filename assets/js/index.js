710a208c268cd1271d8a27843031eb76adf7160c
Kyrylo <fresh.js.ftd.@gmail.com>
2023-04-11 20:14:24 +0300

    Add solutino and continue

--- assets/js/index.js
+++ assets/js/index.js
@@ -18,9 +18,29 @@
 //   console.log('ітерація 2');
 // }
 
-let shouldContinue = true;
-while (shouldContinue) {
-  console.log('ітерація');
+// let shouldContinue = true;
+// while (shouldContinue) {
+//   console.log('ітерація');
 
-  shouldContinue = confirm('Продовжувати цикл?');
+//   shouldContinue = confirm('Продовжувати цикл?');
+// }
+/*
+  у циклі залогуйте в консолі парні числа від 0 до 10 
+*/
+
+let counter = 0;
+// while (counter <= 10) {
+//   console.log(counter);
+
+//   counter += 2;
+// }
+while (counter <= 10) {
+  counter++;
+
+  if(counter % 2 !== 0) {
+    continue; // достроково перейти на наступну ітерацію
+  }
+
+  console.log(counter);
+
 }
