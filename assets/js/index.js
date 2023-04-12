16b0bdd7e93ea8ec94eab79200627768c6b0c108
Kyrylo <fresh.js.ftd.@gmail.com>
2023-04-12 19:33:49 +0300

    Add object creation example

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,26 +5,40 @@ const obj = {
 };
 
 const vukladach = {
-  name : 'Kyrylo',
+  name: 'Kyrylo',
   height: 3.5,
   weight: 1500,
   isMale: true,
   pc: {
-    cpu : {
+    cpu: {
       manufacturer: 'AMD',
-      model: 'Ryzen 2700x'
+      model: 'Ryzen 2700x',
     },
-    ram: 16 
+    ram: 16,
   },
   password: 'admin',
-  email: 'test@test.test'
-}
+  email: 'test@test.test',
+};
 
 const phone = {
   color: 'red',
   price: 15000,
   manufacturer: 'Somebody',
   call: function (somebody) {
-    console.log('calling '+ somebody)
+    console.log('calling ' + somebody);
   },
+};
+// Способи створення об'єктів
+const obj1 = {}; // літеральне
+const obj2 = Object();
+const obj3 = new Object();
+
+const cat = {
+  color: 'black',
+  weight: 9,
+  breed: 'Maine Coon',
+  nickname: 'Ceaser',
+  age: 5,
+  isSleeping: false,
 }
+
