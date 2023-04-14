1444d6433cb4938c031a7ee74ac6b7ccc32a5e81
Kyrylo <fresh.js.ftd.@gmail.com>
2023-04-14 20:04:35 +0300

    Add array creation example

---
+++ assets/js/index.js
@@ -0,0 +1,32 @@
+// const user1Email = 'test@test.com';
+// const user2Email = 'test2@test.com';
+// const user3Email = 'test3@test.com';
+
+const userEmails = {
+  0: 'test@test.com',
+  1: 'test2@test.com',
+  2: 'test3@test.com',
+  3: 'test4@test.com',
+};
+
+const userEmailsArr = new Array(
+  'test@test.com',
+  'test2@test.com',
+  'test3@test.com',
+  'test4@test.com'
+);
+
+// console.log(userEmails);
+// console.log(userEmailsArr);
+
+
+// Array.isArray(userEmailsArr)
+
+const names = [
+  'Anton',
+  'Victoria',
+  'Maxim',
+  'Oleg',
+  'Tetiana',
+];
+
