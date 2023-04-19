b4966f36bcd9c648f8aa7011e90541da0dffd9ff
Kyrylo <fresh.js.ftd.@gmail.com>
2023-04-19 19:59:36 +0300

    Add some tasks with user array

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,19 +2,19 @@
 // const user2Email = 'test2@test.com';
 // const user3Email = 'test3@test.com';
 
-const userEmails = {
-  0: 'test@test.com',
-  1: 'test2@test.com',
-  2: 'test3@test.com',
-  3: 'test4@test.com',
-};
+// const userEmails = {
+//   0: 'test@test.com',
+//   1: 'test2@test.com',
+//   2: 'test3@test.com',
+//   3: 'test4@test.com',
+// };
 
-const userEmailsArr = new Array(
-  'test@test.com',
-  'test2@test.com',
-  'test3@test.com',
-  'test4@test.com'
-);
+// const userEmailsArr = new Array(
+//   'test@test.com',
+//   'test2@test.com',
+//   'test3@test.com',
+//   'test4@test.com'
+// );
 
 // console.log(userEmails);
 // console.log(userEmailsArr);
@@ -210,3 +210,21 @@ function createUsers(amount) {
 }
 
 const userArray1 = createUsers(100);
+
+console.table(userArray1);
+
+// отримати масив пошт користувачів
+const userEmails = userArray1.map(function (user, i) {
+  return user.email;
+});
+
+// отримати масив повнолітніх користувачів
+const adultUsers = userArray1.filter(function (user) {
+  return user.age >= 18;
+});
+
+// отримати масив повнолітніх чоловіків без підписки
+const resultArray = userArray1.filter(function (user) {
+  // return user.age >= 18 && user.isMale === true && user.isSubscribed === false;
+  return user.age >= 18 && user.isMale && !user.isSubscribed;
+});
