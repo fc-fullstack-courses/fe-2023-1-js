37320783af722b89ddeb70de07d1d2b1c587ba49
Kyrylo <fresh.js.ftd.@gmail.com>
2023-04-20 18:37:49 +0300

    Add more prototype examples

--- assets/js/index.js
+++ assets/js/index.js
@@ -25,7 +25,6 @@ const userPrototypeProtype = {
   },
 };
 
-
 userPrototype.__proto__ = userPrototypeProtype;
 user0.__proto__ = userPrototype;
 // user1.__proto__ = userPrototype;
@@ -34,3 +33,78 @@ User.prototype = userPrototype;
 
 const user1 = new User('First User', 50, true);
 const user2 = new User('Second User', 34, false);
+
+// const prototype1 = {
+//   x: function () {},
+//   y: function () {},
+//   abc: 12345
+// };
+
+// const obj1 = {
+//   test1: 123465,
+// };
+
+// obj1.__proto__ = prototype1;
+
+// const me = {
+//   name: 'Test',
+//   age: 15,
+//   hairColor: 'green'
+// };
+
+// const brother = {
+//   name: 'Test brother',
+//   age: 2358,
+// };
+
+// const familyGenes = {
+//   eyeColor: 'blue',
+//   hairColor: 'black',
+//   walk: function () {
+
+//   }
+// };
+
+// me.__proto__ = familyGenes;
+// brother.__proto__ = familyGenes;
+
+function Product(name, price) {
+  this.name = name;
+  this.price = price;
+}
+
+const productPrototype = {
+  name: 'Product',
+  price: 1,
+  buy: function (cost) {
+    // console.log(this);
+    if (cost > this.price) {
+      console.log(`you have bought ${this.name}`);
+    } else {
+      console.log(`Not enought money`);
+    }
+  },
+};
+
+Product.prototype = productPrototype;
+
+const p1 = new Product('P1', 1000);
+
+function Phone(name, price, brand) {
+  this.name = name;
+  this.price = price;
+  this.brand = brand;
+}
+
+const phonePrototype = {
+  call: function (number) {
+    console.log(`You have called ${number}`);
+  },
+};
+
+phonePrototype.__proto__ = productPrototype;
+
+Phone.prototype = phonePrototype;
+
+const phone1 = new Phone('phone', 12354, 'brand');
+
