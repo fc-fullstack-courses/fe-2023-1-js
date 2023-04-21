294e94c29f87d347a861a8976e13eca882df212c
Kyrylo <fresh.js.ftd.@gmail.com>
2023-04-21 19:52:38 +0300

    Add alternative boolean conversion

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,5 +3,10 @@
 const objectStr = new String('test');
 const primitiveStr = 'test';
 
-console.log(objectStr.length);
-console.log(primitiveStr.length);
+// console.log(objectStr.length);
+// console.log(primitiveStr.length);
+
+// перетоврити значення на булеве
+// !!'test'
+const test = Boolean('test');
+console.log(test);
