e8a40b03d9c535299edd720982d802a5d28bbc3a
Kyrylo <fresh.js.ftd.@gmail.com>
2023-04-21 18:54:05 +0300

    Add try catch

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,10 @@
 'use strict';
-
+/**
+ *
+ * @param {number} num1
+ * @param {number} num2
+ * @returns {number}
+ */
 function sum(num1, num2) {
   if (
     typeof num1 !== 'number' ||
@@ -20,6 +25,19 @@ function sum(num1, num2) {
   return num1 + num2;
 }
 
-sum();
+// блок, в якому можливе виникнення помилки
+try {
+  console.log(1);
+  sum(2,2);
+  console.log(2);
+} catch (err) {
+  // виконується якщо в try відбулася помилка
+  // місссція для виправлення наслідків помилки
+  console.log('помилка');
+  console.log(err.message);
+} finally {
+  // блок виконуєтсья після try або після catch
+  console.log('finally');
+}
 
 console.log('test');
