637f444b336be1e36250ca329e6bd4f30ed8e677
Kyrylo <fresh.js.ftd.@gmail.com>
2023-04-21 20:49:35 +0300

    Add strings methods

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,4 +9,24 @@ const primitiveStr = 'test';
 // перетоврити значення на булеве
 // !!'test'
 const test = Boolean('test');
-console.log(test);
+// console.log(test);
+
+const str = 'Test string';
+
+// console.log(str.includes('test'));
+// console.log(str.indexOf('s')); findIndex
+
+// str.toUpperCase()
+// str.toLowerCase()
+
+const badString =
+  '                                                                         Anton                             Antonenko              ';
+const trimmedString = badString.trim();
+
+const fixedString = str.replace('string', 'Null');
+
+const testSubstring = fixedString.substring(0, 4);
+
+const stringsArr = fixedString.split('');
+const testString = 'Word antoher       test 12345. X23';
+const stringsArr2 = testString.split(' ');
