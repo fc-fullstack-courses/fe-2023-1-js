573d3f0950cadf3d192cfd17af331217e32633f5
Kyrylo <fresh.js.ftd.@gmail.com>
2023-04-25 19:24:53 +0300

    Add rest and reduce examples

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,40 @@
 'use strict';
+
+/**
+ *
+ * @param {number} a
+ * @param {number} b
+ * @param {number[]} otherNumbers
+ * @returns
+ */
+const sum = (a, b, ...otherNumbers) => {
+  console.log(otherNumbers);
+  // console.log(arguments);
+  let result = a + b;
+
+  // for (let i = 0; i < otherNumbers.length; i++) {
+  //   result += otherNumbers[i];
+  // }
+
+  return result;
+};
+
+const sum1 = (...numbers) => {
+  //   let result = 0;
+  //   for (let i = 0; i < numbers.length; i++) {
+  //     result += numbers[i];
+  //   }
+  //   return result;
+
+  const result = numbers.reduce((accumulator, number, index, arr) => {
+    return accumulator + number;
+  });
+
+  return result;
+};
+
+const sum2 = (...numbers) => numbers.reduce((acc, number) => acc + number);
+
+const multiply = (...numbers) =>
+  numbers.reduce((acc, number) => acc * number, 1);
+
