b2010db4036bfe537376cbb3f2cb28458cf684ff
Kyrylo <fresh.js.ftd.@gmail.com>
2023-04-26 19:00:58 +0300

    Add basic closure example

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,11 +10,21 @@ function log() {
 function wrapper() {
   let value = 20;
 
+  if (true) {
+    const test1 = 12343;
+    console.log('true');
+  }
+
   console.log(`value in wrapper: ${value}`);
 
   console.log(`log is next`);
 
-
+  // замыкание
+  function log() {
+    // debugger;
+    let test = 50;
+    console.log(`value in log: ${value}`);
+  }
 
   log(); // 10
 }
@@ -24,3 +34,17 @@ function wrapper() {
 wrapper(); // 20
 
 // console.log(`value outside: ${value}`);
+
+function createCounter() {
+  let i = 0; // замкнена змінна для counter
+
+  function counter() {
+    i++;
+    return i;
+  };
+
+  return counter;
+}
+
+const counter1 = createCounter();
+const counter2 = createCounter();
