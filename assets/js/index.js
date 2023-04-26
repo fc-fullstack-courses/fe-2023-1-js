07e953affff820d7c49ddb0e8a417ac5831a38d4
Kyrylo <fresh.js.ftd.@gmail.com>
2023-04-26 20:01:54 +0300

    Add first solution

--- assets/js/index.js
+++ assets/js/index.js
@@ -37,17 +37,43 @@ wrapper(); // 20
 
 function createCounter() {
   let i = 0; // замкнена змінна для counter
-  
+
   return {
     increment: function () {
-
       return ++i;
     },
     decrement: function () {
       return --i;
-    }
+    },
   };
 }
 
 const counter1 = createCounter();
 const counter2 = createCounter();
+
+/*
+  написати функцію createAdder 
+  функція приймає число і повертає іншу функцію
+  повернена функція приймає число і повинна складати його
+  з аргументом createAdder
+
+  const adder = createAdder(10)  
+  adder(30) // 40
+  adder(20) // 60
+  adder(40) // 100
+*/
+
+function createAdder(number) {
+  let accumulator = number;
+
+  function adder(num) {
+    return (accumulator += num);
+  }
+
+  return adder;
+}
+
+const adder = createAdder(10);
+const res1 = adder(30); // 40
+const res2 = adder(20); // 60
+const res3 = adder(40); // 100
