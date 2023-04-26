e320677dd3814b3f012c3d7188183655ccbb54ca
Kyrylo <fresh.js.ftd.@gmail.com>
2023-04-26 18:32:11 +0300

    Add lexical enviroment example

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,26 @@
 'use strict';
+
+let value = 10;
+
+function log() {
+  let test = 50;
+  console.log(`value in log: ${value}`);
+}
+
+function wrapper() {
+  let value = 20;
+
+  console.log(`value in wrapper: ${value}`);
+
+  console.log(`log is next`);
+
+
+
+  log(); // 10
+}
+
+// log(); // 10
+
+wrapper(); // 20
+
+// console.log(`value outside: ${value}`);
