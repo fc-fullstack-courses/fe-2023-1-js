d77769ec856c5a0a493a7e09cbc16d3a063b0b21
Kyrylo <fresh.js.ftd.@gmail.com>
2023-04-27 19:15:34 +0300

    Add recursive tree solution

--- assets/js/index.js
+++ assets/js/index.js
@@ -22,9 +22,48 @@ function countdown(number) {
 function logRange(start, end) {
   console.log(start);
 
-  if(start < end) {
+  if (start < end) {
     // debugger;
     logRange(start + 1, end);
   }
 }
 
+const tree = {
+  value: 10,
+  left: {
+    value: 5,
+    left: {
+      value: 2,
+    },
+    right: {
+      value: 3,
+    },
+  },
+  right: {
+    value: 4,
+    left: {
+      value: 2,
+      right: {
+        value: 1,
+        left: {
+          value: 4,
+        },
+      },
+    },
+  },
+};
+
+function reduceTree (tree) {
+  debugger;
+  let result = tree.value;
+
+  if(tree.left) {
+    result += reduceTree(tree.left);
+  }
+
+  if(tree.right) {
+    result += reduceTree(tree.right);
+  }
+
+  return result;
+}
