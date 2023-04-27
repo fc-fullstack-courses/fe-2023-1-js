a49d9e99a4d5cc34e8dcbf0b303fb2be1c05ad44
Kyrylo <fresh.js.ftd.@gmail.com>
2023-04-27 18:34:31 +0300

    Add recursion function example

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,20 @@
 'use strict';
+
+// let i = 0;
+
+// function test() {
+//   i++;
+//   // debugger;
+//   test();
+// }
+
+function countdown (number) {
+  // debugger;
+  console.log(number);
+  if(number > 0) {
+    // рекурсивний випадок
+    countdown(number - 1);
+  }
+
+  return 'end'; // база рекурсії
+}
