2b40a23965652e59ef77be751a302e52dbd95ec5
Kyrylo <fresh.js.ftd.@gmail.com>
2023-04-28 19:32:40 +0300

    Add Worker task solution

--- assets/js/index.js
+++ assets/js/index.js
@@ -16,3 +16,54 @@ class Cat {
 }
 
 const cat1 = new Cat('test', 'test', 5);
+
+/*
+  Створити класс Worker
+  у робітника має бути:
+    ім'я
+    прізвище
+    вік
+    досвід роботи
+
+    у робітника мають бути методи:
+    метод отримання повного імені (ім'я + прізвище)
+    метод перевірки повноліття 
+*/
+
+class Worker {
+  constructor(firstName, lastName, age, yearsOfExpirience) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.yearsOfExpirience = yearsOfExpirience;
+  }
+
+  getFullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+
+  isAdult() {
+    // if (this.age >= 18) {
+    //   return `${this.firstName} ${this.lastName} is adult`;
+    // }
+    // return `${this.firstName} ${this.lastName} isn't adult`;
+    // if (this.age >= 18) {
+    //   return true;
+    // }
+
+    // return false;
+
+    return this.age >= 18;
+  }
+}
+
+const w1 = new Worker('1', '1', 20, 1);
+const w2 = new Worker('2', '2', 5, 2);
+
+// if(w1.isAdult() === '1 1 is adult' ) {
+//   alert('You are adult')
+// }
+
+// if (w2.isAdult()) {
+//   alert('You are adult');
+// }
