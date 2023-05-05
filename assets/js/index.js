45f67225b34a784342b98d340ddf75b3438ec9ab
Kyrylo <fresh.js.ftd.@gmail.com>
2023-05-05 18:50:40 +0300

    Add example of getting symbol from object

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,19 @@
 'use strict';
 
-const symbol1 = Symbol('test');
+// const symbol1 = Symbol('test');
 
-const symbol2 = Symbol('test');
+// const symbol2 = Symbol('test');
 
-console.log(symbol1 === symbol2);
+// console.log(symbol1 === symbol2);
 
 const user = {
   userName: 'aonfeuf2324',
   password: '39j20jn483gndlsdnf48tqjd094',
   email: 'test@test.test',
-  [symbol1]: 'secret',
+  [Symbol('test')]: 'secret',
   0: 'asdasf',
 };
+
+// const userSymbols = Object.getOwnPropertySymbols(user);
+// user[userSymbols[0]]
+const [symbol1] = Object.getOwnPropertySymbols(user);
