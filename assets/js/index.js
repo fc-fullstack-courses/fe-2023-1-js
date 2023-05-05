780e00f7ccbc0cd3f4efdce2ad7cc1cf325e5f7d
Kyrylo <fresh.js.ftd.@gmail.com>
2023-05-05 18:43:45 +0300

    Add symbol as key example

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,15 @@
 'use strict';
 
 const symbol1 = Symbol('test');
-const symbol2 = Symbol('unit');
+
+const symbol2 = Symbol('test');
 
 console.log(symbol1 === symbol2);
+
+const user = {
+  userName: 'aonfeuf2324',
+  password: '39j20jn483gndlsdnf48tqjd094',
+  email: 'test@test.test',
+  [symbol1]: 'secret',
+  0: 'asdasf',
+};
