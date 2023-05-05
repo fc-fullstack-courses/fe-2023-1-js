0c2d3784c451d1d5713b465e77b22b95de144949
Kyrylo <fresh.js.ftd.@gmail.com>
2023-05-05 20:34:04 +0300

    Add MyArray Iterator example

--- assets/js/index.js
+++ assets/js/index.js
@@ -26,7 +26,19 @@ class MyArray {
     return undefined;
   }
 
-  
+  [Symbol.iterator] () {
+    let i = 0;
+    const context = this;
+    return {
+      next: function () {
+
+        return {
+          done: i >= context.length,
+          value: context[i++],
+        }
+      }
+    }
+  }
 }
 
 const myArr1 = new MyArray();
@@ -34,8 +46,9 @@ myArr1.push(1);
 myArr1.push(5);
 myArr1.push(8);
 
-// for (const number of myArr1) {
-//   console.log(number);
-// }
+for (const number of myArr1) {
+  console.log(number);
+}
 
-const arr = [];
+const arr = [1,3,4];
+const iter = arr[Symbol.iterator]();
