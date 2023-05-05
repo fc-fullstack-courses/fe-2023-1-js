f315094ff691f8e7e177506515fcab33c449e02c
Kyrylo <fresh.js.ftd.@gmail.com>
2023-05-05 19:55:29 +0300

    Add for .. of example

--- assets/js/index.js
+++ assets/js/index.js
@@ -17,3 +17,32 @@ const user = {
 // const userSymbols = Object.getOwnPropertySymbols(user);
 // user[userSymbols[0]]
 const [symbol1] = Object.getOwnPropertySymbols(user);
+
+const arr = [1, 2, 45, 3, 6, 98];
+
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+
+for (const number of arr) {
+  console.log(number);
+}
+
+/*
+  створіть функцію, яка приймає необмежену кількість параметрів (чисел)
+  перемножте всі числа і поверніть результат.
+  Використовувати цикл for .. of
+
+  2,3,4 => 24
+  5,5,10, 10 => 2500
+*/
+
+function multiply (...numbers) {
+  let result = 1;
+
+  for(const number of numbers) {
+    result *= number;
+  }
+
+  return result;
+}
