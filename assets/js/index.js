5b5ce98ffa783a630a2d6ed9b9080a046291d701
Kyrylo <fresh.js.ftd.@gmail.com>
2023-05-10 19:59:09 +0300

    Add button click handler example

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,17 @@
 'use strict';
+
+// window // глобальний об`єкт
+
+// DOM
+// window.document
+// document
+
+function btnAlert() {
+  alert('Hello World! We pressed the Button');
+}
+
+// const buttons = document.getElementsByTagName('button');
+const [firstButton] = document.getElementsByTagName('button');
+
+// target.addEventListener(type, listener);
+firstButton.addEventListener('click', btnAlert );
