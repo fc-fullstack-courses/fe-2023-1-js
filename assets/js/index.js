c60b5ffa7a83731e8174bd78303684df51bd94b7
Kyrylo <fresh.js.ftd.@gmail.com>
2023-05-16 20:49:54 +0300

    Add unfinished dynamic rendering of comments

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,23 @@
 'use strict';
+
+// 1. знайти контейнер для коментів
+const commentsContainer = document.querySelector('#root');
+
+//  2. динамічно створити верстку для кожного об'єкту в масиві
+//    2.1 створити функцію яка буде перетворювати об'єкт на html-елемент
+
+function createUserComment(comment) {
+  const liElement = document.createElement('li');
+
+  // ...
+
+  return liElement;
+}
+
+//    2.2 викликати функцію з 2.1 для кожного коментаря і зберігаємо результат
+const HTMLLiElements = responseData.map((comment) =>
+  createUserComment(comment)
+);
+
+// 3. розмістити отриману верстку в контейнері
+commentsContainer.append(...HTMLLiElements);
