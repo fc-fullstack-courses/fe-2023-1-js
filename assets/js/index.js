36675c843437dd43d5abd03eddab645bfb65507f
Kyrylo <fresh.js.ftd.@gmail.com>
2023-05-17 18:26:28 +0300

    Add basic createUserComment variant

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,10 +7,56 @@ const commentsContainer = document.querySelector('#root');
 //    2.1 створити функцію яка буде перетворювати об'єкт на html-елемент
 
 function createUserComment(comment) {
+  const {
+    authorImg,
+    authorName: authorNameString,
+    title,
+    mark: markText,
+    text,
+  } = comment;
+
   const liElement = document.createElement('li');
+  liElement.classList.add('commentWrapper');
 
-  // ...
+  const article = document.createElement('article');
+  article.classList.add('commentArticle');
 
+  const authorData = document.createElement('div');
+  authorData.classList.add('authorData');
+
+  const imageWrapper = document.createElement('div');
+  imageWrapper.classList.add('imageWrapper');
+
+  const image = document.createElement('img');
+  image.classList.add('authorImg');
+  // image.setAttribute('src', authorImg);
+  image.src = authorImg;
+  image.alt = authorNameString;
+
+  const authorName = document.createElement('p');
+  authorName.classList.add('authorName');
+  authorName.textContent = authorNameString;
+
+  const commentBody = document.createElement('div');
+  commentBody.classList.add('commentBody');
+
+  const commentTitle = document.createElement('h2');
+  commentTitle.classList.add('commentTitle');
+  commentTitle.textContent = title;
+
+  const mark = document.createElement('p');
+  mark.classList.add('mark');
+  mark.textContent = `${markText} / 10`;
+
+  const commentText = document.createElement('p');
+  commentText.classList.add('commentText');
+  commentText.textContent = text;
+
+  commentBody.append(commentTitle, mark, commentText);
+  imageWrapper.append(image);
+  authorData.append(imageWrapper, authorName);
+  article.append(authorData, commentBody);
+  liElement.append(article);
   return liElement;
 }
 
