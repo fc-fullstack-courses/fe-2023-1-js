d58d6e5eb037afbaf08fd10062be3cea5ca42690
Kyrylo <fresh.js.ftd.@gmail.com>
2023-05-18 19:35:14 +0300

    Add setInterval example

--- assets/js/index.js
+++ assets/js/index.js
@@ -21,19 +21,28 @@ function callback2() {
   console.log(`timeout 0.2`);
   console.log(`timeout 0.2 2`);
   // async
-  setTimeout(() => console.log(`timeout 0.2 2 xxasd`),0)
+  // setTimeout(() => console.log(`timeout 0.2 2 xxasd`),0)
   //sync
   console.log(`timeout 0.2 3`);
 }
 
-setTimeout(() => {
-  console.log('timeout 1000');
+// setTimeout повертає id таймера
+const timeout1 = setTimeout(callback2, 0);
+// зупинити таймер з певним id
+clearTimeout(timeout1);
+
+const intervalId = setInterval(() => {
+  console.log('interval');
 }, 1000);
 
-setTimeout(callback2, 0);
+// clearInterval(intervalId);
 
-setTimeout(() => {
-  console.log(`timeout 0`);
-}, 0);
+const ADMIN_PASSWORD = '12345';
 
-console.log('log');
+const intervalId2 = setInterval(() => {
+  const userPass = prompt('password');
+
+  if (userPass === ADMIN_PASSWORD) {
+    clearInterval(intervalId2);
+  }
+}, 5000);
